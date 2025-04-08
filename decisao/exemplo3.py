bc0e616f4a95c6ec8953735aeb036da42b1799f4
luwiz <le9851928@gmail.com>
2025-04-08 20:33:54 -0300

    exercicio if e else

--- decisao/exemplo3.py
+++ decisao/exemplo3.py
@@ -1,1 +1,10 @@
-print('ola')
+# Solicitar um numerto e verificar se o número é par ou impar
+
+numero = int(input('Digite um numero: '))
+
+if numero % 2 == 0:   # True
+    print('Número é par')
+else:                 # False
+    print('Número é impar')
+
+print('Programa continua...')
