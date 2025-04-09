aa4679933fa5b79e922def6a60aaf075ae5c74a2
luwiz <le9851928@gmail.com>
2025-04-09 10:11:13 -0300

    exercicios de loops

---
+++ loops/exemplo1.py
@@ -0,0 +1,14 @@
+# Solicitar 10 numeros e verificar se sao pares ou impares
+
+contador = 1
+while contador <= 10:
+
+    numero = int(input('Digite um numero'))
+    if numero % 2 == 0:
+        print('Par')
+    else:
+        print('Impar')
+
+    contador += 1       # contador = contador + 1
+
+print('Fim')
---
+++ loops/exemplo2.py
@@ -0,0 +1,9 @@
+# Solicitar nomes e idades de 5 pessoas
+
+while True:
+    nome = input('Digite seu nome (Digite sair para finalizar): ')
+    if nome == "SAIR":
+        break
+    idade = int(input('Digite a sua idade:'))
+
+    contador += 1
