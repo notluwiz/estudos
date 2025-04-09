cc9eb19ba5780e3c119cf90e8203e2d82b0b79ec
luwiz <le9851928@gmail.com>
2025-04-08 23:43:06 -0300

    exercicio 4 da live

---
+++ decisao/exemplo4.py
@@ -0,0 +1,16 @@
+
+# Solicitar 2 notas de um aluno e verificar a media
+# Media >= 6
+
+nota1 = float(input('Digite a primeira nota: '))
+nota2 = float(input('Digite a segunda nota: '))
+
+media = (nota1 + nota2) / 2
+
+if media >= 6:  # True
+    print(f'A sua media foi de {media:.2f} e foi aprovado.')
+else:
+    print('Que pena.')
+    print(f'Aluno reprovado com a nota {media:.2f}')
+
+print('Programa continua...')
